26bcc491558bf7c6659194aeff5b90eaf2ae5219
BEMY1 <phalat.tah@gmail.com>
2024-06-03 20:36:58 +0700

    Create file

---
+++ Pro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string name;
+    int num = 10;
+    cout << "Enter name : ";
+    cin >> name;
+    cout << "Hello "<< name << "\t"<< num << endl;
+}
